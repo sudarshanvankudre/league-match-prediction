b18bb4138b1f931f61eb9e8c1cbaf460e8897bf6
sudarshan <joecurious7@gmail.com>
2020-05-18 17:40:06 -0700

    Migrated summoner loading from sqlite to mongodb

--- load_summoners.py
+++ load_summoners.py
@@ -1,10 +1,7 @@
 import requests
-import sqlite3
 import yaml
 import time
-
-# data model : (summonerId text, summonerName text, leaguePoints text, rank text, wins text, losses text, veteran text,
-#         inactive text, freshBlood text, hotStreak text)
+import pymongo
 
 with open("config.yml", 'r') as config:
     config_data = yaml.safe_load(config)
@@ -12,21 +9,13 @@ API_URL = config_data["request_info"]["API_URL"]
 API_KEY = config_data["request_info"]["API_KEY"]
 REQUEST_HEADERS = config_data["request_info"]["REQUEST_HEADERS"]
 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-try:
-    c.execute(
-        '''CREATE TABLE summoners
-            (accountId text, profileIconId text, revisionDate text, name text, id text, puuid text, summonerLevel text)'''
-    )
-except sqlite3.OperationalError:
-    pass
+db = pymongo.MongoClient().riot_data
 
-entries = [row for row in c.execute('SELECT * FROM entries')]
+entries = list(db.entries.find({}))
 count = 0
 while len(entries) > 0:
     entry = entries.pop()
-    summoner_id = entry[0]
+    summoner_id = entry["summonerId"]
     summoner_request = requests.get(API_URL + "/lol/summoner/v4/summoners/" + summoner_id, headers=REQUEST_HEADERS)
     if summoner_request.status_code == 429:
         print("Total summoners processed: {}".format(count))
@@ -38,15 +27,5 @@ while len(entries) > 0:
     else:
         count += 1
         summoner = summoner_request.json()
-        account_id = summoner["accountId"]
-        profile_icon_id = summoner["profileIconId"]
-        revision_date = summoner["revisionDate"]
-        name = summoner["name"]
-        id = summoner["id"]
-        puuid = summoner["puuid"]
-        summoner_level = summoner["summonerLevel"]
-        insert = (account_id, profile_icon_id, revision_date, name, id, puuid, summoner_level)
-        c.execute('INSERT INTO summoners VALUES (?, ?, ?, ?, ?, ?, ?)', insert)
-        conn.commit()
+        db.summoners.insert_one(summoner)
 print(count)
-conn.close()
