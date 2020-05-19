89e59125af949cc6ccc53ff355e643d5485e44fe
sudarshan <joecurious7@gmail.com>
2020-05-18 17:11:54 -0700

    Changed load_entries.py to use mongodb instead of sqlite

--- load_entries.py
+++ load_entries.py
@@ -1,16 +1,8 @@
 import requests
-import pickle
 import yaml
-import sqlite3
+import pymongo
 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-c.execute(
-    '''CREATE TABLE entries
-        (summonerId text, summonerName text, leaguePoints text, rank text, wins text, losses text, veteran text,
-        inactive text, freshBlood text, hotStreak text)'''
-)
-
+db = pymongo.MongoClient().riot_data
 
 with open("config.yml", 'r') as config:
     config_data = yaml.safe_load(config)
@@ -23,19 +15,4 @@ r = requests.get(API_URL + "/lol/league/v4/challengerleagues/by-queue/RANKED_SOL
                  headers=REQUEST_HEADERS)
 
 entries = r.json()["entries"]
-for entry in entries:
-    summoner_id = entry["summonerId"]
-    summoner_name = entry["summonerName"]
-    league_points = entry["leaguePoints"]
-    rank = entry["rank"]
-    wins = entry["wins"]
-    losses = entry["losses"]
-    veteran = entry["veteran"]
-    inactive = entry["inactive"]
-    fresh_blood = entry["freshBlood"]
-    hot_streak = entry["hotStreak"]
-    insert = (summoner_id, summoner_name, league_points, rank, wins, losses, veteran, inactive, fresh_blood, hot_streak)
-    c.execute('INSERT INTO entries VALUES (?,?,?,?,?,?,?,?,?,?)', insert)
-    conn.commit()
-
-conn.close()
+result = db.entries.insert_many(entries)
