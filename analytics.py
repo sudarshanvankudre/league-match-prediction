e03d6ad20ce8e4d50f07ec41e750e57b91ad9901
sudarshan <joecurious7@gmail.com>
2020-06-07 14:16:06 -0700

    Finished function to create summoner winrates collection in database.

--- analytics.py
+++ analytics.py
@@ -1,14 +1,14 @@
 import pymongo
+from data_loader import load_into
 
 db = pymongo.MongoClient().datastore
-
 
 
 def load_winrates(summoner_collection_name, games_collection_name, mode="CLASSIC"):
     """Adds a winrate collection to db that has all of the winrates for each summoner in the summoner_collection_name
     collection using the data from the games_collection_name collection.
     """
-    def game_check(game):
+    def is_valid_game(game):
         if game["gameMode"] != mode:
             return False
         participating = False
@@ -26,9 +26,22 @@ def load_winrates(summoner_collection_name, games_collection_name, mode="CLASSIC
             account_id = summoner["accountId"]
             wins = 0
             total = 0
-            for game in filter(game_check, games):
+            for game in filter(is_valid_game, games):
                 total += 1
+                for p in game["participantIdentities"]:
+                    if p["player"]["accountId"] == account_id:
+                        participant_id = p["participantId"]
+                win = False
+                for p in game["participants"]:
+                    if p["participantId"] == participant_id and p["stats"]["win"]:
+                        win = True
+                        break
+                if win:
+                    wins += 1
 
+            winrate = wins / total
+            doc = {account_id: winrate}
+            load_into("mongodb", doc, "summoner_stats")
     except Exception as e:
         print(e)
 
