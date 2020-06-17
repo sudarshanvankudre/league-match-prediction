17d8eb31aebc78d178ac772b14774f78388eb429
sudarshan <joecurious7@gmail.com>
2020-06-17 14:45:29 -0700

    Now using data from challenger, grandmaster, master, and diamond tiers

--- pull_data.py
+++ pull_data.py
@@ -1,5 +1,11 @@
 from leaguepredict.match_prediction.raw_data_loader import load_entries, load_summoners, load_games
 
-load_entries()
+load_entries("challenger")
+load_entries("grandmaster")
+load_entries("master")
+load_entries("diamond", division="I")
+load_entries("diamond", division="II")
+load_entries("diamond", division="III")
+load_entries("diamond", division="IV")
 load_summoners()
 load_games()
